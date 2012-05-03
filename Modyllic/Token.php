cd968fec8778067b225d9baba7f400e26437900f
Rebecca Turner <me+github@referencethis.com>
2012-05-03 17:03:24 -0400

    Move Modyllic_Token out of Modyllic_Tokenizer

---
+++ Modyllic/Token.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © 2012 Online Buddies, Inc. - All Rights Reserved
+ *
+ * @package Modyllic
+ * @author bturner@online-buddies.com
+ */
+
+/**
+ * These are what are returned by the tokenizer
+ */
+class Modyllic_Token {
+    public $pos;
+    protected $value;
+    function __construct($pos,$value=null) {
+        $this->pos = $pos;
+        $this->value = $value;
+    }
+    /**
+     * This is exactly the string that was matched by the tokenizer
+     */
+    function literal() {
+        return $this->value;
+    }
+    /**
+     * The value of the token.  This differs from literal for things like
+     * comments, which don't include the comment markers and quoted
+     * identifiers which have their quotes removed.
+     */
+    function value() {
+        return $this->value;
+    }
+    /**
+     * The "token" value, for any kind of identifier this is all caps.
+     */
+    function token() {
+        return $this->value;
+    }
+    /**
+     * The token value and class, used in debugging
+     */
+   function debug() {
+       return get_class($this).":'".$this->value."'";
+   }
+}
--- Modyllic/Tokenizer.php
+++ Modyllic/Tokenizer.php
@@ -7,44 +7,8 @@
  */
 
 require_once "Modyllic/SQL.php";
+require_once "Modyllic/Token.php";
 
-/**
- * These are what are returned by the tokenizer
- */
-class Modyllic_Token {
-    public $pos;
-    protected $value;
-    function __construct($pos,$value=null) {
-        $this->pos = $pos;
-        $this->value = $value;
-    }
-    /**
-     * This is exactly the string that was matched by the tokenizer
-     */
-    function literal() {
-        return $this->value;
-    }
-    /**
-     * The value of the token.  This differs from literal for things like
-     * comments, which don't include the comment markers and quoted
-     * identifiers which have their quotes removed.
-     */
-    function value() {
-        return $this->value;
-    }
-    /**
-     * The "token" value, for any kind of identifier this is all caps.
-     */
-    function token() {
-        return $this->value;
-    }
-    /**
-     * The token value and class, used in debugging
-     */
-   function debug() {
-       return get_class($this).":'".$this->value."'";
-   }
-}
 /**
  * Whitespace chunks, including newlines
  */
