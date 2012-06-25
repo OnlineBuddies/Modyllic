164268032e75a047d580e0188329962c5f1657f4
Rebecca Turner <turner@mikomi.org>
2012-06-25 12:16:45 -0400

    Fix normalization for dates and years the way we did for nums

--- Modyllic/Type/Year.php
+++ Modyllic/Type/Year.php
@@ -31,20 +31,14 @@ class Modyllic_Type_Year extends Modyllic_Type {
         $this->length = $old->length;
     }
     function normalize($year) {
-        if ( $year instanceOf Modyllic_Token_Reserved ) {
-            return $year->value();
+        $is_object = is_object($year);
+        $value = $is_object ? $year->value() : $year;
+        $unquoted = $is_object ? $year->unquote() : $year;
+        if ( $year instanceOf Modyllic_Token_Reserved or (!$is_object and Modyllic_SQL::is_reserved($value) ) ) {
+            return $value;
         }
-        if ( $year instanceOf Modyllic_Token_String ) {
-            $plain = $year->unquote() + 0;
-            if ( $plain >= 0 and $plain < 70 ) {
-                return "'20$plain'";
-            }
-            else if ( $plain >= 70 and $plain < 100 ) {
-                return "'19$plain'";
-            }
-        }
-        else if ( $year instanceOf Modyllic_Token_Num ) {
-            $plain = $year->value() + 0;
+        if ( $year instanceOf Modyllic_Token_Num or (!$is_object and is_num($year)) ) {
+            $plain = $value + 0;
             if ( $plain == 0 ) {
                 return "'0000'";
             }
@@ -56,6 +50,15 @@ class Modyllic_Type_Year extends Modyllic_Type {
             }
             else if ( $plain > 1900 and $plain < 2155 ) {
                 return "'$plain'";
+            }
+        }
+        else if ( !$is_object or $year instanceOf Modyllic_Token_String ) {
+            $plain = $unquoted + 0;
+            if ( $plain >= 0 and $plain < 70 ) {
+                return "'20$plain'";
+            }
+            else if ( $plain >= 70 and $plain < 100 ) {
+                return "'19$plain'";
             }
         }
         throw new Exception( "Expected a valid year, got: ".$year->debug() );
